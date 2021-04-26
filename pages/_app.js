ec122b766dc2f2a4b2f2245989567758b695c74f
Marek Szuba <mszuba01@gmail.com>
2021-04-26 23:39:12 +0200

    add vanta logic/sharedLayout from framer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,64 @@
-import '../styles/globals.css'
+import { useEffect, useState, useRef } from 'react';
+import { AnimateSharedLayout } from 'framer-motion';
+import NET from 'vanta/dist/vanta.net.min';
+import * as THREE from 'three';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: vantaRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          color: 0x8e8e8e,
+          backgroundColor: 0x211e2a,
+          scale: 1.0,
+          scaleMobile: 1.0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
+  useEffect(() => {
+    const threeScript = document.createElement('script');
+    threeScript.setAttribute('id', 'threeScript');
+    threeScript.setAttribute(
+      'src',
+      'https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js'
+    );
+    document.getElementsByTagName('head')[0].appendChild(threeScript);
+    return () => {
+      if (threeScript) {
+        threeScript.remove();
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      ref={vantaRef}
+      style={{
+        width: '100%',
+        minHeight: '100vh',
+        backgroundColor: 'rgba(33,30,42)',
+      }}>
+      <AnimateSharedLayout>
+        <Component {...pageProps} />
+      </AnimateSharedLayout>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
