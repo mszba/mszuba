f47f0954702456e280ecbac6732f7d6daa53b1f8
Marek Szuba <mszuba01@gmail.com>
2021-04-27 23:51:29 +0200

    add pages components

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import Link from 'next/link';
-import { motion } from 'framer-motion';
+import AboutMePage from '../components/AboutMePage';
 import DefaultLayout from '../layouts/DefaultLayout';
 
 const AboutMe = () => {
   return (
     <DefaultLayout>
-      <motion.h1 style={{ fontSize: '23px', color: '#fff' }} layoutId='title'>
-        Test of the animated Title about me
-      </motion.h1>
-      <Link href='/'>
-        <a style={{ color: '#fff' }}>HOME</a>
-      </Link>
+      <AboutMePage />
     </DefaultLayout>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import { motion } from 'framer-motion';
-import styles from '../styles/Home.module.css';
+import HomePage from '../components/HomePage';
+
 import DefaultLayout from '../layouts/DefaultLayout';
 
 export default function Home() {
@@ -11,19 +10,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
-      <main
-        className={styles.main}
-        style={{ position: 'absolute', left: 0, right: 0 }}>
-        <motion.h1
-          style={{ fontSize: '43px', color: '#ffff' }}
-          layoutId='title'>
-          Test of the animated Title
-        </motion.h1>
-        <Link href='/aboutme'>
-          <a style={{ color: '#fff' }}>ABOUT ME</a>
-        </Link>
-      </main>
+      <HomePage />
     </DefaultLayout>
   );
 }
