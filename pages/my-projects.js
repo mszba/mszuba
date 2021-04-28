a6c9e824a4e773dd1f62d98afa28d34f8faadbfb
Marek Szuba <mszuba01@gmail.com>
2021-04-28 23:57:59 +0200

    add my-projects to page catalog

---
+++ pages/my-projects.js
@@ -0,0 +1,12 @@
+import ProjectsPage from '../components/ProjectsPage';
+import DefaultLayout from '../layouts/DefaultLayout';
+
+const MyProjects = () => {
+  return (
+    <DefaultLayout>
+      <ProjectsPage />
+    </DefaultLayout>
+  );
+};
+
+export default MyProjects;
