ed1b1d471322820f774987f56365f07c44859073
Marek Szuba <mszuba01@gmail.com>
2021-04-29 23:48:03 +0200

    add diff background color

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,8 @@ function MyApp({ Component, pageProps }) {
 
           color: 0x7f0931,
           backgroundColor: 0x211e2a,
+          // color: 0x3fe8ff,
+          // backgroundColor: 0xf2a52,
           scale: 1.0,
           scaleMobile: 1.0,
         })
