0f1660feb2107d73977554e162cbf77145e878e5
Marek Szuba <mszuba01@gmail.com>
2021-05-02 23:17:43 +0200

    add data for social links

---
+++ components/HomePage/homePageData.js
@@ -0,0 +1,23 @@
+import { ImGithub, ImLinkedin, ImDownload } from 'react-icons/im';
+
+export const socialsData = [
+  {
+    icon: <ImGithub />,
+    iconName: 'github',
+    text: 'mszba',
+    urlAddress: 'https://github.com/mszba',
+  },
+  {
+    icon: <ImLinkedin />,
+    iconName: 'linkedin',
+    text: 'in/marekszuba',
+    urlAddress: 'https://www.linkedin.com/in/marekszuba/',
+  },
+  {
+    icon: <ImDownload />,
+    iconName: 'downloadIcon',
+    text: 'download my CV',
+    urlAddress:
+      'https://drive.google.com/uc?export=download&id=17T5JtMAslVqZFUIiskoK1cqbxUcqvHeZ',
+  },
+];
