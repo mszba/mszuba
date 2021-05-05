363b16cda20f8995b659d93ab73ff76d376d5b5a
Marek Szuba <mszuba01@gmail.com>
2021-05-05 23:42:49 +0200

    add technologies animations and update data

--- components/TechnologiesPage/index.js
+++ components/TechnologiesPage/index.js
@@ -11,17 +11,35 @@ const TechnologiesPage = () => {
         <motion.h1 className={styles.title} layoutId='technologiesTitle'>
           Technologies
         </motion.h1>
-        <h2 className={styles.subTitleText}>
+        <motion.h2
+          className={styles.subTitleText}
+          initial={{ opacity: 0, y: 60 }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            duration: 0.6,
+            delay: 0.4,
+            ease: [0.165, 0.84, 0.44, 1],
+          }}>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
           obcaecati autem consequuntur odit nostrum nemo quo at libero.
-        </h2>
+        </motion.h2>
       </header>
       <main className={styles.main}>
         {technologiesData.map((technology, i) => (
           <motion.div
             key={i}
             layoutId={technology.layoutId}
-            className={styles.element}></motion.div>
+            className={styles.element}>
+            <img
+              className={styles.elementImg}
+              src={technology.src}
+              alt={technology.layoutId}
+            />
+            <span>{technology.layoutId}</span>
+          </motion.div>
         ))}
       </main>
     </SubpageLayout>
--- components/TechnologiesPage/technologiesData.js
+++ components/TechnologiesPage/technologiesData.js
@@ -1,18 +1,111 @@
 import styles from '../HomePage/HomePage.module.css';
 
 export const technologiesData = [
-  { layoutId: 'javascript', className: styles.element1 },
-  { layoutId: 'react', className: styles.element2 },
-  { layoutId: 'css', className: styles.element3 },
-  { layoutId: 'redux', className: styles.element4 },
-  { layoutId: 'styled', className: styles.element5 },
-  { layoutId: 'material', className: styles.element6 },
-  { layoutId: 'postman', className: styles.element7 },
-  { layoutId: 'typescript', className: styles.element8 },
-  { layoutId: 'npm', className: styles.element9 },
-  { layoutId: 'next.js', className: styles.element10 },
-  { layoutId: 'sass', className: styles.element11 },
-  { layoutId: 'docker', className: styles.element12 },
-  { layoutId: 'firebase', className: styles.element13 },
-  { layoutId: 'express', className: styles.element14 },
+  {
+    layoutId: 'JavaScript',
+    className: styles.element1,
+    src:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png',
+  },
+  {
+    layoutId: 'HTML5',
+    className: styles.element2,
+    src:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/600px-HTML5_Badge.svg.png',
+  },
+  {
+    layoutId: 'CSS3',
+    className: styles.element3,
+    src:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Devicon-css3-plain.svg/1200px-Devicon-css3-plain.svg.png',
+  },
+  {
+    layoutId: 'React',
+    className: styles.element4,
+    src: 'https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg',
+  },
+  {
+    layoutId: 'Next.js',
+    className: styles.element5,
+    src:
+      'https://seeklogo.com/images/N/next-js-logo-8FCFF51DD2-seeklogo.com.png',
+  },
+  {
+    layoutId: 'Redux',
+    className: styles.element6,
+    src: 'https://cdn.iconscout.com/icon/free/png-512/redux-283024.png',
+  },
+  {
+    layoutId: 'npm',
+    className: styles.element7,
+    src: 'https://cdn.freebiesupply.com/logos/thumbs/2x/npm-logo.png',
+  },
+  {
+    layoutId: 'axios',
+    className: styles.element8,
+    src: 'https://axios-http.com/assets/logo.svg',
+  },
+  {
+    layoutId: 'REST API',
+    className: styles.element9,
+    src: 'https://i0.wp.com/restfulapi.net/wp-content/uploads/rest.png?ssl=1',
+  },
+  {
+    layoutId: 'Postman',
+    className: styles.element10,
+    src: 'https://iconape.com/wp-content/png_logo_vector/postman.png',
+  },
+  {
+    layoutId: 'JSON',
+    className: styles.element11,
+    src:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/JSON_vector_logo.svg/160px-JSON_vector_logo.svg.png',
+  },
+  {
+    layoutId: 'RWD',
+    className: styles.element12,
+    src:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Responsive_Web_Design_Logo.svg/512px-Responsive_Web_Design_Logo.svg.png',
+  },
+  {
+    layoutId: 'styled-components',
+    className: styles.element13,
+    src: 'https://styled-components.com/logo.png',
+  },
+  {
+    layoutId: 'Material-UI',
+    className: styles.element14,
+    src:
+      'https://seeklogo.com/images/M/material-ui-logo-5BDCB9BA8F-seeklogo.com.png',
+  },
+  {
+    layoutId: 'Node.js',
+    className: styles.element15,
+    src: 'https://www.vectorlogo.zone/logos/nodejs/nodejs-icon.svg',
+  },
+  {
+    layoutId: 'Express.js',
+    className: styles.element16,
+    src: 'https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg',
+  },
+  {
+    layoutId: 'firebase / firestore',
+    className: styles.element17,
+    src: 'https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg',
+  },
+  {
+    layoutId: 'MongoDB',
+    className: styles.element18,
+    src: 'https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg',
+  },
+  {
+    layoutId: 'Git / Github',
+    className: styles.element19,
+    src: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg',
+  },
+  {
+    layoutId: 'Docker',
+    className: styles.element20,
+    src: 'https://www.vectorlogo.zone/logos/docker/docker-icon.svg',
+  },
 ];
