5743551609207b28aeb149c38e5b02bfcf5348e9
Marek Szuba <mszuba01@gmail.com>
2021-05-05 23:42:12 +0200

    add animations and update design concepts

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -5,10 +5,17 @@ import styles from './HomePage.module.css';
 
 import { technologiesData } from '../TechnologiesPage/technologiesData';
 import { socialsData } from './homePageData';
+import { socialAnimations, textAnimations } from './animations';
 
 const helloArray = ['H', 'e', 'l', 'l', 'o', ','];
 
 const HomePage = () => {
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
   return (
     <div className={styles.container}>
       {technologiesData.map((technology, i) => (
@@ -20,66 +27,157 @@ const HomePage = () => {
 
       <div className={styles.wrapper}>
         <header className={styles.header}>
-          <motion.h1 className={styles.title} layoutId='homeTitle'>
-            {helloArray.map((letter, i) => (
-              <motion.span
-                key={i}
-                whileHover={{ scale: 1.2 }}
-                className={styles.singleLetter}>
-                {letter}
-              </motion.span>
-            ))}
+          <motion.h1 className={styles.title}>
+            <motion.span
+              className={styles.singleLetter}
+              initial='out'
+              animate='in'
+              variants={textAnimations}
+              transition={{
+                duration: 0.57,
+                delay: 1.68,
+                ease: [0.165, 0.84, 0.44, 1],
+              }}>
+              H
+            </motion.span>
+            <motion.span
+              className={styles.singleLetter}
+              initial='out'
+              animate='in'
+              variants={textAnimations}
+              transition={{
+                duration: 0.25,
+                delay: 1.45,
+                ease: [0.165, 0.84, 0.44, 1],
+              }}>
+              e
+            </motion.span>
+            <motion.span
+              className={styles.singleLetter}
+              initial='out'
+              animate='in'
+              variants={textAnimations}
+              transition={{
+                duration: 0.7,
+                delay: 1.7,
+                ease: [0.165, 0.84, 0.44, 1],
+              }}>
+              l
+            </motion.span>
+            <motion.span
+              className={styles.singleLetter}
+              initial='out'
+              animate='in'
+              variants={textAnimations}
+              transition={{
+                duration: 0.2,
+                delay: 1.2,
+                ease: [0.165, 0.84, 0.44, 1],
+              }}>
+              l
+            </motion.span>
+            <motion.span
+              className={styles.singleLetter}
+              initial='out'
+              animate='in'
+              variants={textAnimations}
+              transition={{
+                duration: 0.4,
+                delay: 1.6,
+                ease: [0.165, 0.84, 0.44, 1],
+              }}>
+              o
+            </motion.span>
+            <motion.span
+              className={styles.singleLetter}
+              initial='out'
+              animate='in'
+              variants={textAnimations}
+              transition={{
+                duration: 0.3,
+                delay: 1.6,
+                ease: [0.165, 0.84, 0.44, 1],
+              }}>
+              ,
+            </motion.span>
             <br />
+            <motion.span className={styles.singleLetter}>I</motion.span>{' '}
+            <motion.span className={styles.singleLetter}>a</motion.span>
+            <motion.span className={styles.singleLetter}>m</motion.span>{' '}
             <motion.span
-              whileHover={{ scale: 1.2 }}
-              className={styles.singleLetter}>
-              I
-            </motion.span>
-            <motion.span
-              whileHover={{ scale: 1.2 }}
-              className={styles.singleLetter}>
-              '
-            </motion.span>
-            <motion.span
-              whileHover={{ scale: 1.2 }}
-              whileHover={{ scale: 1.2 }}
-              className={styles.singleLetter}>
-              m
-            </motion.span>{' '}
-            <motion.span
-              whileHover={{ scale: 1.2 }}
-              className={styles.singleLetter}>
+              className={styles.singleLetter}
+              initial='out'
+              animate='in'
+              variants={textAnimations}
+              transition={{
+                duration: 0.4,
+                delay: 1.3,
+                ease: [0.165, 0.84, 0.44, 1],
+              }}>
               M
             </motion.span>
             <motion.span
-              whileHover={{ scale: 1.2 }}
-              className={styles.singleLetter}>
+              className={styles.singleLetter}
+              initial='out'
+              animate='in'
+              variants={textAnimations}
+              transition={{
+                duration: 0.5,
+                delay: 1.5,
+                ease: [0.165, 0.84, 0.44, 1],
+              }}>
               a
             </motion.span>
             <motion.span
-              whileHover={{ scale: 1.2 }}
-              className={styles.singleLetter}>
+              className={styles.singleLetter}
+              initial='out'
+              animate='in'
+              variants={textAnimations}
+              transition={{
+                duration: 0.8,
+                delay: 1.3,
+                ease: [0.165, 0.84, 0.44, 1],
+              }}>
               r
             </motion.span>
             <motion.span
-              whileHover={{ scale: 1.2 }}
-              className={styles.singleLetter}>
+              className={styles.singleLetter}
+              initial='out'
+              animate='in'
+              variants={textAnimations}
+              transition={{
+                duration: 0.6,
+                delay: 1.6,
+                ease: [0.165, 0.84, 0.44, 1],
+              }}>
               k
             </motion.span>
             <motion.span
-              whileHover={{ scale: 1.2 }}
               className={styles.pictureDot}
               layoutId='picture'></motion.span>
             <br />
             <motion.span
-              // whileHover={{ scale: 1.2 }}
-              className={styles.frontendText}>
+              className={styles.frontendText}
+              initial='out'
+              animate='in'
+              variants={{
+                in: {
+                  opacity: 1,
+                  x: 0,
+                },
+                out: { opacity: 0, x: -60 },
+              }}
+              transition={{
+                duration: 0.6,
+                delay: 0.2,
+                ease: [0.165, 0.84, 0.44, 1],
+              }}>
               Frontend Developer
             </motion.span>
           </motion.h1>
         </header>
         <nav className={styles.nav}>
-          <ul className={styles.list}>
+          <motion.ul className={styles.list}>
             <motion.li className={styles.listElement} layoutId='aboutmeTitle'>
               <Link href='/aboutme'>
                 <a>about me</a>
@@ -102,10 +200,20 @@ const HomePage = () => {
                 <a>contact me</a>
               </Link>
             </motion.li>
-          </ul>
-          <ul className={styles.externalList}>
+          </motion.ul>
+          <motion.ul className={styles.externalList}>
             {socialsData.map((social, i) => (
-              <li key={i} id={social.iconName}>
+              <motion.li
+                key={i}
+                id={social.iconName}
+                initial='out'
+                animate='in'
+                variants={socialAnimations}
+                transition={{
+                  duration: 1,
+                  delay: i * 0.5,
+                  ease: [0.165, 0.84, 0.44, 1],
+                }}>
                 <a
                   className='social-btn flex-center'
                   href={social.urlAddress}
@@ -113,9 +221,9 @@ const HomePage = () => {
                   {social.icon}
                   <span className='social-text'>{social.text}</span>
                 </a>
-              </li>
+              </motion.li>
             ))}
-          </ul>
+          </motion.ul>
         </nav>
       </div>
     </div>
