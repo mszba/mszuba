ed7f2dbc2e7501c88d9e49e2df8fd2060b3f06da
Marek Szuba <mszuba01@gmail.com>
2021-05-05 23:44:37 +0200

    add animations and reveal components

---
+++ components/HomePage/animations.js
@@ -0,0 +1,16 @@
+export const socialAnimations = {
+  in: {
+    opacity: 1,
+    x: 0,
+  },
+  out: { opacity: 0, x: 30 },
+};
+
+export const textAnimations = {
+  in: {
+    opacity: 1,
+  },
+  out: {
+    opacity: 0,
+  },
+};
--- components/ProjectsPage/index.js
+++ components/ProjectsPage/index.js
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import SubpageLayout from '../../layouts/SubpageLayout';
+import { Fade, Slide, Zoom } from 'react-awesome-reveal';
 import { ImGithub, ImLink } from 'react-icons/im';
 import styles from './ProjectsPage.module.css';
 
@@ -10,128 +11,161 @@ const ProjectsPage = () => {
         <motion.h1 className={styles.title} layoutId='projectsTitle'>
           My projects
         </motion.h1>
-        <h2 className={styles.subTitleText}>
+        <motion.h2
+          className={styles.subTitleText}
+          initial={{ opacity: 0, y: 60 }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            duration: 0.6,
+            delay: 0.4,
+            ease: [0.165, 0.84, 0.44, 1],
+          }}>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
           obcaecati autem consequuntur odit nostrum nemo quo at libero.
-        </h2>
+        </motion.h2>
       </header>
       <main className={styles.main}>
         <article className={styles.projectElement}>
           <div className={styles.projectTextWrap}>
-            <h3 className={styles.projectTitle}>
-              Senter Web App - dolor sit amet consectetur adipisicing elit
-            </h3>
-            <p className={styles.projectText}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequatur quas magnam eaque consequuntur iure doloribus a totam
-              adipisci ullam! Quibusdam. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Dolores tempore. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Consequatur quas magnam eaque
-              consequuntur iure doloribus a totam adipisci ullam! Quibusdam.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-              tempore. Lorem ipsum dolor sit.
-            </p>
+            <Fade triggerOnce cascade delay={800}>
+              <h3 className={styles.projectTitle}>
+                Senter Web App - dolor sit amet consectetur adipisicing elit
+              </h3>
+
+              <p className={styles.projectText}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequatur quas magnam eaque consequuntur iure doloribus a
+                totam adipisci ullam! Quibusdam. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Dolores tempore. Lorem ipsum dolor
+                sit amet consectetur adipisicing elit. Consequatur quas magnam
+                eaque consequuntur iure doloribus a totam adipisci ullam!
+                Quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Dolores tempore. Lorem ipsum dolor sit.
+              </p>
+            </Fade>
             <ul className={styles.projectLinks}>
-              <li id='github'>
-                <a
-                  className='social-btn flex-center social-btn-margin'
-                  href='github.com'
-                  target='_blank'>
-                  <ImGithub />
-                  <span className='social-text'>github</span>
-                </a>
-              </li>
-              <li id='link'>
-                <a
-                  className='social-btn flex-center '
-                  href='github.com'
-                  target='_blank'>
-                  <ImLink />
-                  <span className='social-text'>link</span>
-                </a>
-              </li>
+              <Fade triggerOnce delay={900} cascade direction='right'>
+                <li id='github'>
+                  <a
+                    className='social-btn flex-center social-btn-margin'
+                    href='github.com'
+                    target='_blank'>
+                    <ImGithub />
+                    <span className='social-text'>github</span>
+                  </a>
+                </li>
+                <li id='link'>
+                  <a
+                    className='social-btn flex-center '
+                    href='github.com'
+                    target='_blank'>
+                    <ImLink />
+                    <span className='social-text'>link</span>
+                  </a>
+                </li>
+              </Fade>
             </ul>
           </div>
-          <div className={styles.projectImgWrap}>
-            <div className={styles.projectImg}></div>
-          </div>
+          <Fade triggerOnce delay={800}>
+            <div className={styles.projectImgWrap}>
+              <div className={styles.projectImg}></div>
+            </div>
+          </Fade>
         </article>
 
         <article
           className={`${styles.projectElement} ${styles.projectElementReverse}`}>
           <div
             className={`${styles.projectTextWrap} ${styles.projectTextWrapReverse}`}>
-            <h3 className={styles.projectTitle}>
-              School Web App - dolor sit amet consectetur adipisicing elit
-            </h3>
-            <p className={styles.projectText}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequatur quas magnam eaque consequuntur iure doloribus a totam
-              adipisci ullam! Quibusdam. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Dolores tempore. Lorem ipsum dolor sit.
-            </p>
+            <Fade triggerOnce delay={600} cascade>
+              <h3 className={styles.projectTitle}>
+                School Web App - dolor sit amet consectetur adipisicing elit
+              </h3>
+              <p className={styles.projectText}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequatur quas magnam eaque consequuntur iure doloribus a
+                totam adipisci ullam! Quibusdam. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Dolores tempore. Lorem ipsum dolor
+                sit.
+              </p>
+            </Fade>
             <ul className={styles.projectLinks}>
-              <li id='github'>
-                <a
-                  className='social-btn flex-center social-btn-margin'
-                  href='github.com'
-                  target='_blank'>
-                  <ImGithub />
-                  <span className='social-text'>github</span>
-                </a>
-              </li>
-              <li id='link'>
-                <a
-                  className='social-btn flex-center '
-                  href='github.com'
-                  target='_blank'>
-                  <ImLink />
-                  <span className='social-text'>link</span>
-                </a>
-              </li>
+              <Fade triggerOnce cascade delay={800} cascade direction='right'>
+                <li id='github'>
+                  <a
+                    className='social-btn flex-center social-btn-margin'
+                    href='github.com'
+                    target='_blank'>
+                    <ImGithub />
+                    <span className='social-text'>github</span>
+                  </a>
+                </li>
+                <li id='link'>
+                  <a
+                    className='social-btn flex-center '
+                    href='github.com'
+                    target='_blank'>
+                    <ImLink />
+                    <span className='social-text'>link</span>
+                  </a>
+                </li>
+              </Fade>
             </ul>
           </div>
-          <div
-            className={`${styles.projectImgWrap} ${styles.projectImgWrapReverse}`}>
-            <div className={styles.projectImg}></div>
-          </div>
+          <Fade triggerOnce delay={800}>
+            <div
+              className={`${styles.projectImgWrap} ${styles.projectImgWrapReverse}`}>
+              <div className={styles.projectImg}></div>
+            </div>
+          </Fade>
         </article>
 
         <article className={styles.projectElement}>
           <div className={styles.projectTextWrap}>
-            <h3 className={styles.projectTitle}>
-              School Web App - dolor sit amet consectetur adipisicing elit
-            </h3>
-            <p className={styles.projectText}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequatur quas magnam eaque consequuntur iure doloribus a totam
-              adipisci ullam! Quibusdam. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Dolores tempore. Lorem ipsum dolor sit.
-            </p>
+            <Fade triggerOnce delay={600} cascade>
+              <h3 className={styles.projectTitle}>
+                School Web App - dolor sit amet consectetur adipisicing elit
+              </h3>
+
+              <p className={styles.projectText}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequatur quas magnam eaque consequuntur iure doloribus a
+                totam adipisci ullam! Quibusdam. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Dolores tempore. Lorem ipsum dolor
+                sit.
+              </p>
+            </Fade>
             <ul className={styles.projectLinks}>
-              <li id='github'>
-                <a
-                  className='social-btn flex-center social-btn-margin'
-                  href='github.com'
-                  target='_blank'>
-                  <ImGithub />
-                  <span className='social-text'>github</span>
-                </a>
-              </li>
-              <li id='link'>
-                <a
-                  className='social-btn flex-center '
-                  href='github.com'
-                  target='_blank'>
-                  <ImLink />
-                  <span className='social-text'>link</span>
-                </a>
-              </li>
+              <Fade triggerOnce cascade delay={800} cascade direction='right'>
+                <li id='github'>
+                  <a
+                    className='social-btn flex-center social-btn-margin'
+                    href='github.com'
+                    target='_blank'>
+                    <ImGithub />
+                    <span className='social-text'>github</span>
+                  </a>
+                </li>
+                <li id='link'>
+                  <a
+                    className='social-btn flex-center '
+                    href='github.com'
+                    target='_blank'>
+                    <ImLink />
+                    <span className='social-text'>link</span>
+                  </a>
+                </li>
+              </Fade>
             </ul>
           </div>
-          <div className={styles.projectImgWrap}>
-            <div className={styles.projectImg}></div>
-          </div>
+          <Fade triggerOnce delay={800}>
+            <div className={styles.projectImgWrap}>
+              <div className={styles.projectImg}></div>
+            </div>
+          </Fade>
         </article>
       </main>
     </SubpageLayout>
