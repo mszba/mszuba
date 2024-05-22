bb16890d750c8902376650dfdece8f93c0d250ee
Marek Szuba <mszuba01@gmail.com>
2024-05-22 22:37:03 +0200

    feat: update mailer info

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,20 +1,22 @@
 export default function (req, res) {
   require('dotenv').config();
 
+  const { SMTP_PASSWORD, SMTP_EMAIL, RECEIVER_EMAIL } = process.env;
+
   let nodemailer = require('nodemailer');
   const transporter = nodemailer.createTransport({
     port: 465,
     host: 'smtp.gmail.com',
     auth: {
-      user: 'emailsenderform@gmail.com',
-      pass: process.env.password,
+      user: SMTP_EMAIL,
+      pass: SMTP_PASSWORD,
     },
     secure: true,
   });
 
   const mailData = {
-    from: 'emailsenderform@gmail.com',
-    to: 'mszuba01@gmail.com',
+    from: req.body.email,
+    to: RECEIVER_EMAIL,
     subject: `${req.body.subject} - message from ${req.body.name}`,
     text: req.body.message + ' | Sent from: ' + req.body.email,
     html: `<div>${req.body.message}</div><p>Sent from:
